edf05afc3c2375b88bfc95efd72e8749d51e05fc
ohgun0325 <harrygunoh@gmail.com>
2026-01-07 16:32:59 +0900

    ddl auto 수정

--- src/main/java/site/ohgun/api/MonolithicApplication.java
+++ src/main/java/site/ohgun/api/MonolithicApplication.java
@@ -3,9 +3,12 @@ package site.ohgun.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "site.ohgun.api.user.repository")
 public class MonolithicApplication {
 
     public static void main(String[] args) {
